cffce3f153d531a14f0287b01e9c675f38bb9dfe
kk7679 <kohtakatoh@gmail.com>
2019-07-22 15:37:19 +0900

    first

--- web/application/views.py
+++ web/application/views.py
@@ -1,13 +1,17 @@
+from flask import url_for, render_template
+
 from application import app
+from application import scrap
 
 
 @app.route('/')
 def top_page():
-    hello = "TOP page Hello world !!"
-    return hello
+    title = 'ようこそ日本語!!'
+    msg = 'TEST 一覧ページ'
+    return render_template('index.html', title = title, msg = msg)
 
-
-@app.route('/hello')
-def hello():
-    hello = "Hello world"
-    return hello
+@app.route('/list')
+def show_list():
+    title = "はてな人気リスト"
+    results = scrap.get_hatena_entries('https://b.hatena.ne.jp/hotentry/it')
+    return render_template('list.html', title = title, results = results)
